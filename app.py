50b3cc2a28d16bca1382a5b85d7ec7db8b7429ab
William Surya P <109983782+williamsuryap@users.noreply.github.com>
2025-03-21 11:58:00 +0700

    update correlation matrix

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ elif page == "Environmental Factors":
     st.subheader("Correlation Matrix of Dataset")
     
     # Calculate correlation matrix
-    corr_matrix = df[['Day', 'Month', 'Year', 'Date', 'SST', 'CHL', 'MLD', 'Salin', 'Catch (kg)']].corr()
+    corr_matrix = df[['Day', 'Month', 'Year', 'Date', 'SST', 'CHL', 'MLD', 'Salin','Lat', 'Lon', 'Catch (kg)']].corr()
     
     fig, ax = plt.subplots(figsize=(9, 6))
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5, ax=ax)
